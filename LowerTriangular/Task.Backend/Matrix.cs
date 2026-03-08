38411efe7d7e3c06f9bbbfc54955697fdee34253
Diego Valencia <91valenciadiego@gmail.com>
2026-03-07 23:33:43 -0500

    Write a program that creates a square matrix of order N. The matrix should be filled with the following formula: Cell[i][j] = i + j. Then, it should display the matrix as filled and then display the lower triangular part of the matrix.

---
+++ LowerTriangular/Task.Backend/Matrix.cs
@@ -0,0 +1,68 @@
+﻿namespace Task.Backend
+{
+    public class Matrix
+    {
+        private int _order;
+        private int[,] _matrix;
+
+        public Matrix(int order)
+        {
+            Order = order;
+            _matrix = new int[Order, Order];
+        }
+
+        public int Order
+        {
+            get => _order;
+            set => _order = ValidateOrder(value);
+        }
+
+        private int ValidateOrder(int order)
+        {
+            if (order <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(order),
+                    "The order of the matrix cannot be less than 1");
+            }
+
+            return order;
+        }
+
+        public void FillMatrix()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    _matrix[i, j] = i + j;
+                }
+            }
+        }
+
+        public void ShowMatrix()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    Console.Write($"{_matrix[i, j],4} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowLowerTriangle()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($"{_matrix[i, j],4} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ LowerTriangular/Task.Frontend/Program.cs
@@ -0,0 +1,11 @@
+﻿using Task.Backend;
+
+Matrix matrix = new Matrix(10);
+
+matrix.FillMatrix();
+
+matrix.ShowMatrix();
+
+Console.WriteLine();
+
+matrix.ShowLowerTriangle();
